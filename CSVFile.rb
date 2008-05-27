ea4163eeefb2182074b26d8aca3a75b0e5f7058a
Paul Davis <net.prophet.nz@gmail.com>
2008-05-27 17:23:54 +1200

    writing sql insert statements

--- CSVFile.rb
+++ CSVFile.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
-#CSVFile.rb - script to read flat csv files and write normalised data to load into SQL
+# CSVFile.rb - script to read flat csv files and write normalised data to load into SQL
+# Usage: ruby CSVFile.rb testdata.csv > testdata.sql
 
 ## Pseudocode'ish
 # open ARGV for reading
@@ -9,8 +10,8 @@
 #  read in line
 #  parse into hash with keys from header
 #    convert QDATE value to Date object
-#  for each of 11 data groups
-#    write out line with normalised data
+#  for each of 12 quarterly data groups
+#    write out sql insert line with normalised data
 
 require 'date' # if date values are to be parsed
 
@@ -25,7 +26,7 @@ class CSVFile
     @data = file.collect do |line| # array of CSVLine objects
       NormData.new(header, line)    # NormData is a special case of CSVLine
     end
-    file.close
+    ensure file.close unless file.nil?
     @data.compact! # remove unsuccessful lines
   end
   
@@ -34,7 +35,7 @@ class CSVFile
   end
   
   def to_sql # output an sql input file to stdout
-    
+    "# Outputting to sql from #{@filename}...\n\n" + (@data.collect { |line| if line.valid? then line.to_sql end }).join("\n")
   end
 end
 
@@ -46,6 +47,14 @@ class CSVLine
     values = csv_line.chomp!.split(',')
     values.each_with_index { |v, i| @data[keys[i]] = v }
   end
+  
+  def valid?
+    !(@data.nil? || @data.empty?)
+  end
+
+  def to_sql
+    "No generic sql statement supplied."
+  end
 
 end
 
@@ -53,33 +62,43 @@ class NormData < CSVLine # this sub-class is data specific
   
   def initialize(keys, csv_line) # create normalised data structure from CSV data hash
     super(keys, csv_line) # hash of key/value pairs for this line
-    print "Parsing code #{@data["CODE"]}..."
     @code = @data["CODE"]
     @name = @data["NAME"]
     date = Date.parse(@data["QDATE"],true) # parses an excel type date
-    @report = []
-    @report << [ReportData.new(date, @data["SALQ"], @data["COSTQ"], @data["CPTQ"])]
+    @reports = []
+    @reports << ReportData.new(date, @data["SALQ"], @data["COSTQ"], @data["CPTQ"])
     1.upto(11) do |q| # for each quarter in the data
-      @report << ReportData.new(date << q * 3, @data["SALQ" + q.to_s], @data["COSTQ" + q.to_s], @data["CPTQ" + q.to_s]) 
+      @reports << ReportData.new(date << q * 3, @data["SALQ" + q.to_s], @data["COSTQ" + q.to_s], @data["CPTQ" + q.to_s]) 
     end
-    @report.compact!
-    puts "done."
-    rescue: puts "Unable to parse record for code #{@data["CODE"]}..."; return nil
+    @reports.compact!
+    rescue: puts "# Unable to parse record for code #{@data["CODE"]}..."; return nil
+  end
+  
+  def valid? 
+    !(@reports.nil? || @reports.empty?)
   end
   
   def to_s
     "\nCode: #{@code} Name: #{@name}\n" + (
-      if @report.nil? || @report.length == 0 then
-        "  No reports.\n"
+      if @reports.to_a != [] then
+        @reports.join("\n")
       else
-        @report.join("\n")
+        "No reports."
       end
     )
   end
   
+  def to_sql
+    sql_data = "# (#{@code}): #{@name}\n"; prefix = "REPLACE INTO `history` (`code`,`name`,`result_date`,`sales`,`gross_profit`,`diluted_shares`)\n"
+    (@reports.each { |r| sql_data << prefix << "  VALUES ('#{@code}', '#{@name}', #{r.to_sql});\n"})
+    sql_data
+  end
+
 end
 
 class ReportData
+
+  attr_reader :date, :sales, :cogs, :shares
 
   def initialize(date, sales, cogs, shares)
     @date = date; @sales = (sales.to_f * 1e6); @cogs = cogs.to_f * 1e6; @shares = shares.to_i * 1e3
@@ -89,6 +108,14 @@ class ReportData
   def to_s
     "Date: #{@date.to_s} Sales: #{@sales.to_s} COGS: #{@cogs.to_s} Shares: #{@shares.to_s}"
   end
+  
+  def to_sql
+    if @shares.nil? then
+      "'#{@date}', '#{@sales}', '#{@sales - @cogs}', DEFAULT"
+    else
+      "'#{@date}', '#{@sales}', '#{@sales - @cogs}', '#{@shares}'"
+    end
+  end
 
 end
 
@@ -97,5 +124,5 @@ end
 if $0 == __FILE__ # command line
   unless ARGV.length == 1 then puts "Usage: ruby testdata.rb csvfile.csv"; exit; end
   require 'pp'
-  ARGV.each { |f| csv = CSVFile.new(f); puts csv }
+  ARGV.each { |f| csv = CSVFile.new(f); puts csv.to_sql }
 end
